e02ead792b7419cdbb317415af0185254cb79db0
iamxavia <125671500+iamxavia@users.noreply.github.com>
2023-03-17 22:34:23 +0700

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import sklearn
+import plotly
+model = pickle.load(open("model.pickle", "rb"))
+
+st.title('Revenue Prediction')
+temp = st.number_input('Input Temperature')
+temp= np.array([temp]).reshape(-1,1)
+kq = model.predict(temp)[0]
+
+if st.button("Predict"):
+  st.write("Revenue Prediction")
+  st.write(kq)
